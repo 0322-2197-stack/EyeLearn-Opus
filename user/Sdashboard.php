f9bafab6da0120a2cce347b29d6e015cd9f8a7c6
waweakweak <vonnannilov1031@gmail.com>
2025-12-11 22:27:23 +0800

    Fix profile dropdown z-index to allow clicking logout

--- user/Sdashboard.php
+++ user/Sdashboard.php
@@ -124,7 +124,7 @@ $comprehension_data = getComprehensionLevel($conn, $user_id);
             background-color: white;
             border-radius: 0.5rem;
             box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06);
-            /* z-index: 50; */
+            z-index: 100;
         }
         
         .profile-dropdown.show {
